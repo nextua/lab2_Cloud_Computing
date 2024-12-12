31ed9f71e4837072a734a612bca587de3a2d1606
Vovchik <infix2022@gmail.com>
2024-12-12 15:53:22 +0200

    test

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-import os
 from flask import Flask
-from datetime import datetime
-
 app = Flask(__name__)
 
-@app.route("/")
-def show_date():
-    today_date = datetime.now().strftime("%Y-%m-%d")
-    return f"<h1>Today's Date: {today_date}</h1>"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
